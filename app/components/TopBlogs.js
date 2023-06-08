e1c920f0ddf7568b546a9d4ca23cf05f5986aa53
iambstha <iambstha@gmail.com>
2023-06-08 21:21:14 +0545

    Displayed data from database

--- app/components/TopBlogs.js
+++ app/components/TopBlogs.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
-import data from '../data'
+import getArticles from '../func/getArticles';
 
-const TopBlogs = () => {
-    const dataa = data.slice(0,3)
+const TopBlogs = async () => {
+    const post = await getArticles()
+    const dataa = post.slice(0,3)
   return (
     <div className=' flex justify-center w-full '>
         <div className=' flex flex-col m-2  w-full lg:w-4/5 lg:flex-row lg:m-4 justify-center border-2 border-slate-200 p-4 rounded-md '>
@@ -14,7 +15,7 @@ const TopBlogs = () => {
             {
                 dataa.map(d => {
                     return <Link href={d.title} className=' ml-2 w-full border-b-2 lg:border-b-0 border-b-slate-100 mb-2 lg:mb-0 lg:w-1/4 p-2 pl-4 pr-4 hover:bg-slate-100 transition-colors '>
-                    <span className=' text-slate-400 text-sm ' >{d.tag[0].key}</span>
+                    <span className=' text-slate-400 text-sm ' >{d.tag}</span>
                     <h3 className=' font-semibold ' >{d.title}</h3>
                     <p className=' font-thin text-slate-400 flex flex-wrap text-sm ' >{d.desc}</p>
                 </Link>
